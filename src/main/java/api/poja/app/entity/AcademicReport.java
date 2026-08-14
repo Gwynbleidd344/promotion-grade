73d6e03d0c06ff9c89fb655bc787e2426c3f5f9b
Vaniaina <vanimendes344@gmail.com>
2026-08-14 08:32:10 +0300

    feat: added the general average & total credit into the AcademicReport

--- src/main/java/api/poja/app/entity/AcademicReport.java
+++ src/main/java/api/poja/app/entity/AcademicReport.java
@@ -12,6 +12,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
@@ -53,4 +54,10 @@ public class AcademicReport {
 
   @Column(name = "sent_at")
   private LocalDateTime sentAt;
+
+  @Column(name = "general_average", precision = 5, scale = 2)
+  private BigDecimal generalAverage;
+
+  @Column(name = "total_credits_obtained")
+  private Integer totalCreditsObtained;
 }
