b4aad2752fd0739646d6541fb49c8697d81abf81
Ilo <hei.iloniaina@gmail.com>
2026-08-16 18:16:57 +0300

    chore: removed unsued imports

--- src/main/java/api/poja/app/endpoint/rest/controller/CourseAssignmentController.java
+++ src/main/java/api/poja/app/endpoint/rest/controller/CourseAssignmentController.java
@@ -11,7 +11,6 @@ import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/api/poja/app/endpoint/rest/controller/StudentController.java
+++ src/main/java/api/poja/app/endpoint/rest/controller/StudentController.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
