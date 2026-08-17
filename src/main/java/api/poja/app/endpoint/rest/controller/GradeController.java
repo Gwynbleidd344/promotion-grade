63cc0da532e9ebebd2fdbf66243397a48f24d939
Vaniaina <vanimendes344@gmail.com>
2026-08-17 13:40:17 +0300

    feat: added the PUT /grades/{id} request

--- src/main/java/api/poja/app/endpoint/rest/controller/GradeController.java
+++ src/main/java/api/poja/app/endpoint/rest/controller/GradeController.java
@@ -1,6 +1,7 @@
 package api.poja.app.endpoint.rest.controller;
 
 import api.poja.app.endpoint.rest.dto.GradeCreateRequest;
+import api.poja.app.endpoint.rest.dto.GradeUpdateRequest;
 import api.poja.app.model.Grade;
 import api.poja.app.service.GradeService;
 import jakarta.validation.Valid;
@@ -8,7 +9,11 @@ import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
@@ -20,5 +25,11 @@ public class GradeController {
   public ResponseEntity<Grade> createGrade(
       @PathVariable UUID id, @Valid @RequestBody GradeCreateRequest request) {
     return ResponseEntity.status(HttpStatus.CREATED).body(gradeService.create(id, request));
+  }
+
+  @PutMapping("/api/v1/grades/{id}")
+  public ResponseEntity<Grade> updateGrade(
+      @PathVariable UUID id, @Valid @RequestBody GradeUpdateRequest request) {
+    return ResponseEntity.ok(gradeService.update(id, request));
   }
 }
