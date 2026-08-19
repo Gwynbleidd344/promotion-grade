791fb95301b474ee2fa9aed7699fbe02922e94e0
Ilo <hei.iloniaina@gmail.com>
2026-08-19 10:53:32 +0300

    fix: removed preauthorized from transcriptController

--- src/main/java/api/poja/app/endpoint/rest/controller/TranscriptController.java
+++ src/main/java/api/poja/app/endpoint/rest/controller/TranscriptController.java
@@ -7,7 +7,6 @@ import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,14 +25,12 @@ public class TranscriptController {
     return ResponseEntity.ok(academicReportService.list(id));
   }
 
-  @PreAuthorize("hasRole('ADM')")
   @PostMapping("/{id}/transcripts/{year}/generate")
   public ResponseEntity<AcademicReport> generateTranscript(
       @PathVariable UUID id, @PathVariable String year) {
     return ResponseEntity.status(HttpStatus.CREATED).body(academicReportService.generate(id, year));
   }
 
-  @PreAuthorize("hasRole('ADM')")
   @PostMapping("/{id}/transcripts/{year}/send")
   public ResponseEntity<Void> sendTranscript(@PathVariable UUID id, @PathVariable String year) {
     academicReportService.requestSend(id, year);
